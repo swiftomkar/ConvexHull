0197306d3e591581e10d9ab4baebebaab0e921bc
swiftomkar <swiftomkar@gmail.com>
2018-04-09 11:08:44 -0400

    Conflict graph progress

--- ConvexHull/ConflictGraph.h
+++ ConvexHull/ConflictGraph.h
@@ -7,13 +7,21 @@
 #include "Facet.h"
 #include <iostream>
 #include <vector>
-class ConflictGraph:public Point, public Facet{
+class ConflictGraph:public Point{
 private:
     //for each point; make a list of facets that are in front of it
-    std::vector<int> conflicts;
-
+    std::vector <Facet> ConGraph;
 public:
-    ConflictGraph(Point p){
-        
+    ConflictGraph(Facet p) {
+        UpdateGraph(p);
+    }
+    void UpdateGraph(Facet p){
+        for(int i=0;i<Facet::numFacets();i++) {
+            if (Facet::FacetList[i].Faces(*this))//assuming we have a list of all facets
+                ConGraph.push_back(Facet::FacetList[i]);
+        }
+    }
+    std::vector<Facet> GetConflictGraph(){
+        return this->ConGraph;
     }
 };
